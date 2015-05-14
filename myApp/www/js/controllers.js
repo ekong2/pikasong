74f30f6a294be566d8cd1f4db09bb7ffbf705c7c
Eddie Kong <eddiek93@gmail.com>
2015-05-13 20:16:25 -0700

    song queue responsive, deleting too, pending db

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -3,7 +3,7 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {})
 
 .controller('DjCtrl', function($scope, Songs) {
-
+  $scope.queriedSongs = [];
   $scope.search = {};
 
   $scope.searchSong = function(){
@@ -11,15 +11,31 @@ angular.module('starter.controllers', [])
       client_id: '2e75744c571473e8d226078a69bba4b3'
     });
 
+    $scope.queriedSongs = [];
+
     SC.get('/tracks', { q: $scope.search.query }, function(tracks) {
-      console.log(tracks);
+      $scope.$apply(function(){
+        for (var i = 0; i < tracks.length; i++){
+          var song = tracks[i];
+          $scope.queriedSongs.push({
+            id: song.id,
+            title: song.title,
+            count: song.playback_count,
+            thumbnail: song.artwork_url,
+            year: song.release_year
+          });
+        }
+      });
     });
 
-     $scope.search.query = "";
+    $scope.search.query = "";
   }
 
+  $scope.queueSong = function(song){
+    //Add song to firebase here
+    $scope.queriedSongs.splice($scope.queriedSongs.indexOf(song),1);
+  }
 
-  $scope.songs = Songs.all();
 //   $scope.play = function(id){
 //    SC.stream("/tracks/" + id ,function(sound){
 //       // Save sound, it holds all the data needed to stop, resume, etc.
